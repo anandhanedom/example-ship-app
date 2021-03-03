ddc3a2dcf9358e048479d32c87bf8daa12f6eb6c
Anandhan <anandhanedom@gmail.com>
2021-03-03 23:18:25 +0530

    adads

--- src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-const pages = [
-  { path: "/", groups: ["Developer admin", "admin"] },
-  { path: "shipments", groups: ["Developer admin", "admin"] },
-  { path: "/shipments/:waybillNumber", groups: ["Developer admin", "admin"] },
-];
-
-export default pages;
